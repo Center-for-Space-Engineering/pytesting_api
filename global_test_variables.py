3615fec2ca0a68a7ebb4925d325aa7dc16f22974
CSE  Ground Station <nathan.tipton@arcfield.com>
2024-05-28 11:41:41 -0600

    linting

--- global_test_variables.py
+++ global_test_variables.py
@@ -1,3 +1,7 @@
+'''
+This file contatins the nesccary constants for the unit test to run. Mainly the coms object so the unit test can talk tot the rest of teh system. 
+'''
+
 coms = None #this is how our test will be able  to talk to everything running on our system. 
 db_name = "" #this is the data base name this is so the test can save things into the database. 
 session_id = "" # This variable is so the test know what session the are currently running under this will allow us to correlate testing conditions with test. 
--- tests/test_gps_sensor.py
+++ tests/test_gps_sensor.py
@@ -1,12 +1,25 @@
-import pytest
+'''
+    dummy unit tests
+'''
+
+import pytest #pylint: disable=w0611
 from pytesting_api import global_test_variables # pylint: disable=e0401
 
 
 def test_example():
+    '''
+        Simple example
+    '''
     assert 1 + 1 == 2
 
 def test_example2():
+    '''
+        Failed unit test
+    '''
     assert 1 + 1 == 3
 
 def test_fixture():
+    '''
+        Example of how to call the coms object
+    '''
     assert global_test_variables.coms.get_test() == "testing"

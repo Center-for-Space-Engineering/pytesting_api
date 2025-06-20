fe080d39288dafca5f21ebd3e9561eb5c95323de
Liz <lizolson1000@gmail.com>
2025-06-20 14:41:06 -0600

    made test more readable/lintable

--- testing/tests/global_test_variables_test.py
+++ testing/tests/global_test_variables_test.py
@@ -8,74 +8,63 @@ from logging_system_display_python_api.messageHandler import messageHandler
 @pytest.fixture
 def patch_sleep():
     sleep_patch = patch('time.sleep')
-    sleep_patch.start()
-    yield
+    mock_sleep = sleep_patch.start()
+    yield mock_sleep
     sleep_patch.stop()
 
 @pytest.fixture
 def patch_coms():
-    get_return_patch = patch.object(messageHandler, 'get_return')
-    send_request_patch = patch.object(messageHandler, 'send_request')
-    get_return_patch.start()
-    send_request_patch.start()
-    yield
-    get_return_patch.stop()
-    send_request_patch.stop()
-
-@pytest.fixture
-def setup_mocks(patch_sleep, patch_coms):
-    yield
+    mock_coms_obj = MagicMock(send_request=MagicMock(), get_return=MagicMock())
+    yield mock_coms_obj
 
 @pytest.mark.global_test_variables_tests
-def test_get_last_idx(setup_mocks):
+def test_get_last_idx(patch_sleep, patch_coms):
     # Immediate return
-    mock_coms = messageHandler()
-    mock_coms.send_request.return_value = 1
-    mock_coms.get_return.return_value = 42
+    patch_coms.send_request.return_value = 1
+    patch_coms.get_return.return_value = 42
 
     tester = global_test_variables
-    tester.coms = mock_coms
+    tester.coms = patch_coms
 
     result = tester.get_last_idx('Johnny')
 
     assert result == 42
-    mock_coms.get_return.assert_called_once()
+    patch_coms.get_return.assert_called_once()
 
     # Return after sleep
-    mock_coms.get_return.side_effect = [None, None, 42]
+    patch_coms.get_return.side_effect = [None, None, 42]
 
     result = tester.get_last_idx('Peter')
 
-    assert time.sleep.call_count == 2
-    assert mock_coms.get_return.call_count == 4
+    assert patch_sleep.call_count == 2
+    assert patch_coms.get_return.call_count == 4
 
 @pytest.mark.global_test_variables_tests
-@patch.object(global_test_variables, 'db_name', new=str("Kade"))
-@patch.object(global_test_variables, 'coms', new_callable=messageHandler)
-def test_save_data_to_db(mock_coms, setup_mocks):
+def test_save_data_to_db(patch_coms):
     tester = global_test_variables
+    tester.coms = patch_coms
+    tester.db_name = "Kade"
 
     tester.save_data_to_db('Big Justin', ['data'], 'thread')
-    mock_coms.send_request.assert_called_with('Kade', ['save_data_group', 'Big Justin', ['data'], 'thread'])
+    patch_coms.send_request.assert_called_with('Kade', ['save_data_group', 'Big Justin', ['data'], 'thread'])
 
 @pytest.mark.global_test_variables_tests
-def test_get_data_from_data_base(setup_mocks):
-    mock_coms = messageHandler()
-    mock_coms.send_request.return_value = 2
-    mock_coms.get_return.return_value = 69
+def test_get_data_from_data_base(patch_coms, patch_sleep):
+    patch_coms.send_request.return_value = 2
+    patch_coms.get_return.return_value = 69
 
     tester = global_test_variables
-    tester.coms = mock_coms
+    tester.coms = patch_coms
 
     result = tester.get_data_from_data_base('Todd', 0, 100)
 
     assert result == 69
-    mock_coms.get_return.assert_called_once()
+    patch_coms.get_return.assert_called_once()
 
     # Return after sleep
-    mock_coms.get_return.side_effect = [None, None, 69]
+    patch_coms.get_return.side_effect = [None, None, 69]
 
     result = tester.get_data_from_data_base('Rowan', 3, 14)
 
-    assert time.sleep.call_count == 2
-    assert mock_coms.get_return.call_count == 4
+    assert patch_sleep.call_count == 2
+    assert patch_coms.get_return.call_count == 4
